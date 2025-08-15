a10a900c68fe9f0aa90734eb6bfc4dd59225d6c3
wealipay <1293600794@qq.com>
2025-08-16 06:44:46 +0800

    hhh

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var nextConfig = {
-    /* config options here */
-    ignoreBuildErrors: true
+/* config options here */
 };
 exports.default = nextConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreBuildErrors: true
 };
 
 export default nextConfig;
