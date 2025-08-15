bbfc8f92c7a6b8b202f800838acef455c4701bd8
wealipay <1293600794@qq.com>
2025-08-16 01:30:08 +0800

    hhh

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import LightRays from "./components/reactbits/LightRays";
 export default function Home() {
-  return <div>helggg五点水平hh五年hlo</div>;
+  return (
+    <div style={{ width: "100%", height: "600px", position: "relative" }}>
+      <LightRays
+        raysOrigin="top-center"
+        raysColor="#00ffff"
+        raysSpeed={1.5}
+        lightSpread={0.8}
+        rayLength={1.2}
+        followMouse={true}
+        mouseInfluence={0.1}
+        noiseAmount={0.1}
+        distortion={0.05}
+        className="custom-rays"
+      />
+    </div>
+  );
 }
