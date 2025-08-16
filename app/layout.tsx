81d9603a30feec26935fce79aff5279937f33618
wealipay <1293600794@qq.com>
2025-08-17 05:37:33 +0800

    ghh

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Ma_Shan_Zheng } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const font = Ma_Shan_Zheng({
   weight: "400",
@@ -31,7 +32,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN" className={font.className}>
-      <body className="h-screen">{children}</body>
+      <body>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0"
+          />
+        </Head>
+        {children}
+      </body>
     </html>
   );
 }
