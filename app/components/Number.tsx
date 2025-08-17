250ed32addd9c1f9ea46bc2a04b06de2c7182732
wealipay <1293600794@qq.com>
2025-08-18 01:27:33 +0800

    ghh

--- app/components/Number.tsx
+++ app/components/Number.tsx
@@ -1,15 +1,18 @@
-"use client";
 import GradientText from "./reactbits/GradientText";
 
 export default function Number() {
   return (
-    <GradientText
-      colors={["#ff0000", "#4079ff", "#fff", "#4079ff", "#ff0000"]}
-      animationSpeed={2}
-      showBorder={true}
-      className="animate-gradient"
-    >
-      853688884
-    </GradientText>
+    <div className="flex flex-col justify-center items-center">
+      <span>在支付宝首页搜索</span>
+      <GradientText
+        colors={["#ff0000", "#4079ff", "#fff", "#4079ff", "#ff0000"]}
+        animationSpeed={2}
+        showBorder={true}
+        className="animate-gradient"
+      >
+        853688884
+      </GradientText>
+      <span>即可领红包</span>
+    </div>
   );
 }
