50d14b48375bc0dba94a8483fa3d5a6a17cdc5ee
wealipay <1293600794@qq.com>
2025-08-18 06:40:09 +0800

    ghh

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import StarBorder from "./components/reactbits/StarBorder";
 import Number from "./components/Number";
 import LightRays from "./components/reactbits/LightRays";
 
 export default function Home() {
   return (
     <div className="relative h-1/3">
+      <StarBorder
+        as="button"
+        className="custom-class"
+        color="cyan"
+        speed="5s"
+      >
+        联系
+      </StarBorder>
+
       <LightRays
         raysOrigin="top-center"
         raysColor="#ff0000"
