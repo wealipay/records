eafe008a80d5da3d97e93898b9119d13b426bf85
wealipay <1293600794@qq.com>
2025-08-17 21:27:38 +0800

    ghh

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import Number from './components/Number';
+import Number from "./components/Number";
+import LightRays from "./components/reactbits/LightRays";
 
 export default function Home() {
   return (
     <div>
-       
-<Number />
+      <LightRays
+        raysOrigin="top-center"
+        raysColor="#ff0000"
+        raysSpeed={1}
+        lightSpread={0.5}
+        rayLength={3}
+        followMouse={true}
+        mouseInfluence={1}
+        noiseAmount={0}
+        distortion={0}
+        className="animate-gradient"
+      />
+
+      <Number />
     </div>
   );
 }
