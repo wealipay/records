930ff026edd1491bcca4d276069fdad961895bd2
wealipay <1293600794@qq.com>
2025-08-17 21:06:41 +0800

    ghh

--- app/components/Number.tsx
+++ app/components/Number.tsx
@@ -9,7 +9,7 @@ export default function Number() {
       showBorder={true}
       className="custom-class"
     >
-      Add a splash of color!五
+      Add a splash of color!五王总
     </GradientText>
   );
 }
