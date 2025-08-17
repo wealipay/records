19bf785d64aa3fec1a68a89b05818125823c4fca
wealipay <1293600794@qq.com>
2025-08-17 21:00:55 +0800

    ghh

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+   theme: {
+     extend: {
+       keyframes: {
+         gradient: {
+           '0%': { backgroundPosition: '0% 50%' },
+           '50%': { backgroundPosition: '100% 50%' },
+           '100%': { backgroundPosition: '0% 50%' },
+         },
+       },
+       animation: {
+         gradient: 'gradient 8s linear infinite'
+       },
+     },
+   },
+   plugins: [],
+ };
