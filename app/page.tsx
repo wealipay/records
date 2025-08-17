0447f828e89dffa45a4c34c2c4a14f9723572199
wealipay <1293600794@qq.com>
2025-08-18 02:29:53 +0800

    ghh

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LightRays from "./components/reactbits/LightRays";
 
 export default function Home() {
   return (
-    <div className="relative">
+    <div className="relative h-1/2">
       <LightRays
         raysOrigin="top-center"
         raysColor="#ff0000"
