5ec227f1d4be7c7afe159eda9e7adc8d9a93114f
wealipay <1293600794@qq.com>
2025-08-18 19:25:04 +0800

    dd

--- app/components/Number.tsx
+++ app/components/Number.tsx
@@ -1,6 +1,11 @@
-
+import GradientText from "./reactbits/GradientText";
 export default function Number() {
-  return (
-<div>hell</div>
-  );
+  return <GradientText
+  colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+  animationSpeed={3}
+  showBorder={false}
+  className="custom-class"
+>
+  Add a splash of color!
+</GradientText>;
 }
