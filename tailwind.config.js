6817f9df7286c9400f5526dbe21f3d48bf2067cc
wealipay <1293600794@qq.com>
2025-08-18 09:47:24 +0800

    ghh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-    animation: {
-         'star-movement-bottom': 'star-movement-bottom linear infinite alternate',
-         'star-movement-top': 'star-movement-top linear infinite alternate',
-       },
+   theme: {
+     extend: {
        keyframes: {
-         'star-movement-bottom': {
-           '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
-           '100%': { transform: 'translate(-100%, 0%)', opacity: '0' },
-         },
-         'star-movement-top': {
-           '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
-           '100%': { transform: 'translate(100%, 0%)', opacity: '0' },
+         gradient: {
+           '0%': { backgroundPosition: '0% 50%' },
+           '50%': { backgroundPosition: '100% 50%' },
+           '100%': { backgroundPosition: '0% 50%' },
          },
        },
-    },
-  },
-  plugins: [],
-};
+       animation: {
+         gradient: 'gradient 8s linear infinite'
+       },
+     },
+   },
+   plugins: [],
+ };
